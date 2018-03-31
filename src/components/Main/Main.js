c4955f233249929a2ab471b5becf89c783fe820f
ransdev <jakubtolinski@gmail.com>
2018-03-31 19:00:25 +0200

    change srv

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -307,7 +307,7 @@ class Main extends React.Component {
 
 
 startListen(){
-     fetch('http://credentials-1.mybluemix.net/api/speech-to-text/token')   
+     fetch('http://voice_app.mybluemix.net/api/speech-to-text/token')   
 
   .then(function(response) {
       return response.text();
